e66f2c1dfd0c60dc023f767c6659212b13260838
Vlad Hoida <goyda2000@gmail.com>
2026-07-17 19:30:40 +0300

    style: додано пробіл перед закриваючою кавичкою в атрибуті class

--- resources/views/components/victorian-footer.blade.php
+++ resources/views/components/victorian-footer.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-10 text-center text-[#5d3a1a] text-sm">
+<div class="mt-10 text-center text-[#5d3a1a] text-sm" >
     <p>&copy; {{ date('Y') }} {{ config('app.name', 'Laravel') }}. {{ __('city.all_rights_reserved') }}</p>
     <p class="mt-1">{{ __('city.designed_in_the_spirit') }}</p>
 </div>
